96266a8013c397c647017e56325db5e5793b8e68
Yurii04UA <uyriparaka@gmail.com>
2022-08-29 21:18:58 +0300

    finished

--- js.js
+++ js.js
@@ -1,9 +1,11 @@
 /// Function construction
-function Invoices(name, position, id) {
+function Invoices(name, position, id = 0) {
   this.name = name;
   this.position = position;
   this.id = id;
-  this.report = false; // isReport?
+  this.isReport = false;
+  this.isAdved = false;
+  this.availableAmount = 0;
 
   Invoices.static = function () {
     return "I do not have access to instance";
@@ -17,93 +19,135 @@ Invoices.prototype.getName = function () {
   return `Hello ${this.name}`;
 };
 
-const user = new Invoices('Yurii', 'user', 100)
-console.log(user);
-
-function AccountantInvoice(name, position, id, report) {
-  Invoices.call(this, name, position, id)
-  this.report = report;
+function AccountantInvoice(name, position, id, isReport) {
+  Invoices.call(this, name, position, id);
+  this.isReport = isReport;
 }
 AccountantInvoice.prototype = Object.create(Invoices.prototype);
 AccountantInvoice.prototype.constructor = AccountantInvoice;
 AccountantInvoice.prototype.getReport = function () {
-  return this.report ? `Hello ${this.name} you can get a report` : `Hello ${this.name} you can't get a report`
-}
+  return this.isReport
+    ? `Hello ${this.name} you can get a report`
+    : `Hello ${this.name} you can't get a report`;
+};
 
-const accountant1 = new AccountantInvoice('Sergeevna', 'accountant', 4, true)
-const accountant2 = new AccountantInvoice('Petrovna', 'accountant', 5, true)
-const accountant3 = new AccountantInvoice('Valentinovna', 'accountant', 6, false)
+const accountant1 = new AccountantInvoice("Sergeevna", "accountant", 4, true);
+const accountant2 = new AccountantInvoice("Petrovna", "accountant", 5, true);
+const accountant3 = new AccountantInvoice(
+  "Valentinovna",
+  "accountant",
+  6,
+  false
+);
 console.log(accountant1);
 console.log(accountant1.getInfo());
 console.log(accountant1.getReport());
 console.log(accountant3.getReport());
 
-function CEOInvoice (name, position, id) {
-  Invoices.call(this, name, position, id)
+function CEOInvoice(name, position, id, isAdved) {
+  Invoices.call(this, name, position, id);
+  this.isAdved = isAdved;
 }
 
+CEOInvoice.prototype = Object.create(Invoices.prototype);
+CEOInvoice.prototype.constructor = CEOInvoice;
+CEOInvoice.prototype.printAdved = function () {
+  return this.isAdved
+    ? `Hello ${this.name} you can advertise`
+    : `Hello ${this.name} you can't advertise`;
+};
+
+const seo1 = new CEOInvoice("Ivan", "seo specialist", 21, true);
+const seo2 = new CEOInvoice("Ivanka", "seo specialist", 22, false);
+console.log(seo1);
+console.log(seo1.printAdved());
+console.log(seo2.printAdved());
+
+function CustomerInvoice(name, position, id, availableAmount) {
+  Invoices.call(this, name, position, id);
+  this.availableAmount = availableAmount;
+}
+CustomerInvoice.prototype = Object.create(Invoices.prototype);
+CustomerInvoice.prototype.constructor = CustomerInvoice;
+CustomerInvoice.prototype.printAccount = function () {
+  return `Hello ${this.name} you have ${this.availableAmount}$`;
+};
+
+const customer1 = new CustomerInvoice("Vitaliy", "Customer", 599, 10000);
+const customer2 = new CustomerInvoice("Vitaliy", "Customer", 599, 10000);
+
+console.log(customer1);
+console.log(customer1.getInfo());
+console.log(customer1.printAccount());
 
 /// Class
 console.log("///////////////////////////////////////////////////");
 class ClassInvoices {
-  constructor(name, invoices = "unknown", amount = "unknown") {
+  constructor(name, position, id) {
     this.name = name;
-    this.invoices = invoices;
-    this.amount = amount;
+    this.position = position;
+    this.id = id;
+    this.isReport = false;
+    this.isAdved = false;
+    this.availableAmount = 0;
   }
   getInfo() {
-    return `Hello ${this.name}, your invoice balance #${this.invoices} : ${this.amount}$ `;
+    return `Hello ${this.name}, your position ${this.position}. Id : ${this.id}`;
+  }
+  getName() {
+    return `Hello ${this.name}`;
   }
   static staticMethod() {
     return "I do not have access to instance";
   }
 }
-// console.log("ClassInvoices static method:", ClassInvoices.staticMethod());
 
-const classUser = new ClassInvoices("Yurii", 422, 10000);
-// console.log("classUser :", classUser);
-// console.log("classUser :", classUser.getInfo());
-
-const classAccountantInvoice = new ClassInvoices("Semenovna", 2);
-classAccountantInvoice.position = "Accountant";
-classAccountantInvoice.getInfo = function () {
-  return `Hello ${this.name}, your position :${this.position} `;
-};
-// console.log("classAccountantInvoice :", classAccountantInvoice);
-// console.log("classAccountantInvoice :", classAccountantInvoice.getInfo());
-
-const classSeo = new ClassInvoices("Michalich", 1);
-classSeo.someProperty = "some property";
-classSeo.print = function () {
-  return this.someProperty;
-};
-// console.log("classSeo :", classSeo);
-// console.log("classSeo :", classSeo.getInfo());
-// console.log("classSeo :", classSeo.print());
-
-class ClassOwner extends ClassInvoices {
-  constructor(company) {
-    super("Petrovich"); // ???
-    this.company = company;
+class ClassAccountantInvoice extends ClassInvoices {
+  constructor(name, position, id, isReport) {
+    super(name, position, id);
+    this.isReport = isReport;
+  }
+  getReport() {
+    return this.isReport
+      ? `Hello ${this.name} you can get a report`
+      : `Hello ${this.name} you can't get a report`;
   }
 }
 
-const classOwner = new ClassOwner("RBC");
-classOwner.subordinates = [{
-    name: "Yurii",
-    position: "user",
-  },
-  {
-    name: "Semenovna",
-    position: "Accountant",
-  },
-  {
-    name: "Michalich",
-    position: "seo",
-  },
-];
-classOwner.print = function () {
-  return this.subordinates;
-};
-// console.log("classOwner :", classOwner);
-// console.log("classOwner :", classOwner.subordinates);
+const classAccountant = new ClassAccountantInvoice(
+  "Sergeevna",
+  "accountant",
+  4,
+  true
+);
+
+console.log(classAccountant);
+
+class ClassSEOInvoice extends ClassInvoices {
+  constructor(name, position, id, isAdved) {
+    super(name, position, id);
+    this.isAdved = isAdved;
+  }
+  printAdved() {
+    return this.isAdved
+      ? `Hello ${this.name} you can advertise`
+      : `Hello ${this.name} you can't advertise`;
+  }
+}
+
+const classSeo = new ClassSEOInvoice("Ivan", "seo specialist", 21, true);
+console.log(classSeo);
+
+class ClassCustomerInvoice extends ClassInvoices {
+  constructor(name, position, id, availableAmount) {
+    super(name, position, id);
+    this.availableAmount = availableAmount;
+  }
+  printAccount() {
+    return `Hello ${this.name} you have ${this.availableAmount}$`;
+  }
+}
+
+const classUser = new ClassCustomerInvoice("Vitaliy", "Customer", 599, 10000);
+
+console.log(classUser.printAccount());
